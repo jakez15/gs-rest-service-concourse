5108a702db30aa7689c2fe9c956bf5a278efca33
Jake Zingler <jzingler@pivotal.io>
2017-08-13 15:36:21 -0700

    update to use maven deploy and scm plugin

--- ci/tasks/package.sh
+++ ci/tasks/package.sh
@@ -8,7 +8,7 @@ set -e -x
 
 pushd source-repo
 	mvn versions:set -DnewVersion=${VERSION} -Dmaven.repo.local=../.m2/repository
-	mvn clean package -Dmaven.repo.local=../.m2/repository -DskipTests=true
+	mvn clean deploy scm:tag -Dmaven.repo.local=../.m2/repository -DskipTests=true
 	
 	cp pom.xml ../build-output/pom-${VERSION}.xml
 	cp manifest.yml  ../build-output/manifest-${VERSION}.yml
