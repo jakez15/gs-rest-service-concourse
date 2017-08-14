759bdb9919c992fdebd81765c0719b9903b9f745
Jake Zingler <jzingler@pivotal.io>
2017-08-13 22:04:33 -0700

    add smoke-test

--- ci/tasks/smoke-test.sh
+++ ci/tasks/smoke-test.sh
@@ -3,10 +3,19 @@
 set -e -x
 
 pushd source-repo
-    HEALTH=$(curl https://gs-rest-service-concourse.cfapps.io/health | jq '.status == "UP"')
+	echo "0"
+	curl 'https://gs-rest-service-concourse.cfapps.io/health'
+	echo "1"
+	curl 'https://gs-rest-service-concourse.cfapps.io/health' | jq '.status == "UP"'
+	echo "2"
+	HEALTH=$(curl 'https://gs-rest-service-concourse.cfapps.io/health' | jq '.status == "UP"')
+	
+	echo $HEALTH
+	
 	if [[ true == $HEALTH ]]; then
 		echo "PASSED Smoke Tests"
 	else 
 		echo "FAILED Smoke Tests"
+		return 1
 	fi 	
 popd
